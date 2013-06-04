311542ef379871de3ce882c3ee2ea16ad123f0cf
Forrest L Norvell <forrest@newrelic.com>
2013-06-03 22:45:04 -0700

    Hashing utilities.
    Requires external initialization of TinyMT and doesn't do any allocation
    itself, which is fine, as I want wrk to use static buffers whenever
    possible.

---
+++ src/hash.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "hash.h"
+
+/* Don't care about endianness -- get a uint64_t, return an array of bytes.
+ */
+union ll_to_ba
+{
+  uint8_t byte[8];
+  uint64_t longlong;
+} ll_to_ba;
+
+void sha256(void *msg, size_t msg_len, unsigned char *buf) {
+    SHA_CTX ctx;
+    SHA_INIT(&ctx);
+    SHA_UPDATE(&ctx, msg, msg_len);
+    SHA_FINAL(&ctx, buf);
+}
+
+void hexillate(char *inbuf, char *outbuf) {
+    const static char *hex = "0123456789abcdef";
+
+    unsigned char *in = inbuf;
+    char *out = outbuf;
+    for (int i = 0; i < sizeof(inbuf); i++) {
+        *out++ = hex[(*in >> 4) & 0xf];
+        *out++ = hex[*in++ & 0xf];
+    }
+}
+
+void random_hash(tinymt64_t *state, char *outbuf) {
+    unsigned char buf[SHA_LENGTH];
+    union ll_to_ba thunk;
+    unsigned char w = sizeof(ll_to_ba.byte);
+
+    for (int j = 0; j < SHA_LENGTH / w; j++) {
+        thunk.longlong = tinymt64_generate_uint64(state);
+        for (int i = 0; i < w; i++) {
+            buf[(j * w) + i] = thunk.byte[i];
+	}
+    }
+
+    hexillate(buf, outbuf);
+}
---
+++ src/hash.h
@@ -0,0 +1,22 @@
+#ifdef __APPLE__
+#include <CommonCrypto/CommonDigest.h>
+#define SHA_CTX    CC_SHA256_CTX
+#define SHA_INIT   CC_SHA256_Init
+#define SHA_UPDATE CC_SHA256_Update
+#define SHA_FINAL(CTX, BUF)  CC_SHA256_Final((BUF), (CTX))
+#define SHA_LENGTH CC_SHA256_DIGEST_LENGTH
+#else /* !__APPLE__ */
+#include <bsd/stdlib.h>
+#include "sha2.h"
+#define SHA_CTX    sha256_ctx
+#define SHA_INIT   sha256_init
+#define SHA_UPDATE sha256_update
+#define SHA_FINAL(CTX, BUF)  sha256_final((CTX), (BUF))
+#define SHA_LENGTH SHA256_DIGEST_SIZE
+#endif
+
+#include "tinymt64.h"
+
+void sha256(void *msg, size_t msg_len, unsigned char *buf);
+void hexillate(char *inbuf, char *outbuf);
+void random_hash(tinymt64_t *state, char *outbuf);
